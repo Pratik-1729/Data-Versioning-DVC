11d1e6e6f05b3e60533dfcf6d25aabac7f33b5fd
pratik1729 <gaikwadpratik1729@gmail.com>
2025-07-10 18:06:01 +0530

    Commit before the dvc

---
+++ code_csv.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import os
+data = {
+    'Name':["Alice","Bob","Charlie"],
+    'Age':[20,30,35],
+    'City':["NYC","LA",'DC']
+}
+
+df = pd.DataFrame(data)
+
+# data_dir = "data"
+os.makedirs("data",exist_ok=True)
+file_path = os.path.join("data",'Sample_data.csv')
+
+df.to_csv(file_path,index=False)
+
+print(f"the file is saved at {file_path}")
